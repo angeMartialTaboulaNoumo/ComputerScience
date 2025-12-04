3c9d33b9f9ca7ece4bf5eee1dee2adb66615ec70
brelnosse <brelnosse2@gmail.com>
2025-12-04 23:07:04 +0100

    Implement authentication pages with styling; add Login and Signup components

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Nird from './pages/Nird';
 import GameHome from './pages/GameHome';
 import NotFound from './pages/NotFound';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import './assets/styles/index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,6 +14,8 @@ root.render(
     <Router>
       <Routes>
         <Route path='/gamehome' element={<GameHome />}/>
+        <Route path='/login' element={<Login />}/>
+        <Route path='/signup' element={<Signup />}/>
         <Route path='nird' element={<Nird />}/>
         <Route path='*' element={<NotFound />}/>
       </Routes>
