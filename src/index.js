20b61a3d98815fd6dd18f25928cd2ce3b84feab0
angeMartialTaboulaNoumo <martialtaboula@gmail.com>
2025-12-04 22:50:35 +0100

    Remplacement des composantes de l'acceuil avec des nouvelles structures

--- src/index.js
+++ src/index.js
@@ -2,15 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import GameHome from './pages/GameHome';
-
+import Accueil from './pages/Accueil';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
       <Routes>
+        <Route path="/" element={<Accueil />} />
         <Route path='/gamehome' element={<GameHome />}/>
       </Routes>
     </Router>
   </React.StrictMode>
 );
+
+
 
