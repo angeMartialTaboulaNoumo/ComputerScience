74a2767d73c3a3a49d5d40c828d3dcdef2592c13
brelnosse <brelnosse2@gmail.com>
2025-12-04 22:30:24 +0100

    Add quiz functionality and styling to GameHome, include NotFound page

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import GameHome from './pages/GameHome';
+import NotFound from './pages/NotFound';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -9,6 +10,7 @@ root.render(
     <Router>
       <Routes>
         <Route path='/gamehome' element={<GameHome />}/>
+        <Route path='*' element={<NotFound />}/>
       </Routes>
     </Router>
   </React.StrictMode>
